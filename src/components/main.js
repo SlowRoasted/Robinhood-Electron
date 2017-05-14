4073d8580cf21c5e1a3876f1b01688924c645c02
Pinghao Qi <feifeiqph@gmail.com>
2017-05-14 09:07:32 -0400

    converted main to a full react component, added a placeholder update function in main for future data updates

--- src/components/main.js
+++ src/components/main.js
@@ -6,24 +6,39 @@ import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { Grid, Row, Col } from 'react-flexbox-grid'
 
-export const Main = ({ debug }) => {
-    let debugText = JSON.stringify(debug == '' ? 'Empty Debug' : debug)
-    return (
-        <Grid fluid>
-            <Row>
-                <Col xs={4} md={3}>
-                    <div style={styles.text}>
-                        {debugText}
-                    </div>
-                </Col>
-                <Col xs={8} md={9}>
-                    <div style={styles.text}>
-                        {debugText}
-                    </div>
-                </Col>
-            </Row>
-        </Grid>
-    )
+class Main extends Component {
+    componentDidMount() {
+        const update = () => {
+            console.log('tick')
+            this.timer = setTimeout(update, 500);
+        }
+        update.bind(this)
+        update()
+    }
+
+    componentWillUnmount() {
+        console.log(this.timer)
+        clearTimeout(this.timer);
+    }
+    render() {
+        const { debug } = this.props
+        return (
+            <Grid fluid>
+                <Row>
+                    <Col xs={4} md={3}>
+                        <div style={styles.text}>
+                            {JSON.stringify(debug == '' ? 'Empty Debug' : debug)}
+                        </div>
+                    </Col>
+                    <Col xs={8} md={9}>
+                        <div style={styles.text}>
+                            {JSON.stringify(debug == '' ? 'Empty Debug' : debug)}
+                        </div>
+                    </Col>
+                </Row>
+            </Grid>
+        )
+    }
 }
 const propTypes = {
     debug: PropTypes.string
